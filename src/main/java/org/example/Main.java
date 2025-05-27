2216da4053f14a0f295dc8c279b401f3cb83c3e9
soipanhamisi <soipanhani@gmail.com>
2025-05-27 11:31:36 +0300

    created a function for each feature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,31 @@ public class Main {
         int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 
+        // Print results
+        System.out.println("Total sum of all numbers: " + totalSum(numbers));
+        System.out.println("Sum of even numbers: " + evenSum(numbers));
+        System.out.println("Sum of odd numbers: " + oddSum(numbers));
+    }
+    public static int evenSum(int[] numbers) {
+        int evenSum = 0;
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                evenSum += num;
+            }
+        }
+        return evenSum;
+    }
+    public static int oddSum(int[] numbers) {
+        int oddSum = 0;
+        for (int num : numbers) {
+            if (num % 2 != 0) {
+                oddSum += num;
+            }
+        }
+        return oddSum;
+    }
+
+    public static int totalSum(int[] numbers) {
         int totalSum = 0;
         int evenSum = 0;
         int oddSum = 0;
@@ -13,16 +38,8 @@ public class Main {
         for (int num : numbers) {
             totalSum += num;
 
-            if (num % 2 == 0) {
-                evenSum += num;
-            } else {
-                oddSum += num;
-            }
         }
 
-        // Print results
-        System.out.println("Total sum of all numbers: " + totalSum);
-        System.out.println("Sum of even numbers: " + evenSum);
-        System.out.println("Sum of odd numbers: " + oddSum);
+        return totalSum;
     }
 }
